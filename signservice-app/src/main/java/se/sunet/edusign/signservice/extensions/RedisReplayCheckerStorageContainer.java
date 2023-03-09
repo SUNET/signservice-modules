298d2d7d9f92ce5817fa9761e9bd89d0e273bd0a
Martin Lindström <martin.lindstrom@litsec.se>
2023-03-09 16:28:46 +0100

    Fix for Redis
    Must ensure that the SESSION cookie is sent.

--- signservice-app/src/main/java/se/sunet/edusign/signservice/extensions/RedisReplayCheckerStorageContainer.java
+++ signservice-app/src/main/java/se/sunet/edusign/signservice/extensions/RedisReplayCheckerStorageContainer.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2023 Sweden Connect
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package se.sunet.edusign.signservice.extensions;
 
 import java.time.Duration;
@@ -33,7 +18,7 @@ public class RedisReplayCheckerStorageContainer implements ReplayCheckerStorageC
 
   /** Default element lifetime. */
   public static final Duration DEFAULT_ELEMENT_LIFETIME = Duration.ofMinutes(10);
-  
+
   private static final String REDIS_KEY_PREFIX = "replay-";
 
   /** The container name. */
@@ -41,9 +26,6 @@ public class RedisReplayCheckerStorageContainer implements ReplayCheckerStorageC
 
   /** The Redis template object. */
   private final RedisTemplate<String, Long> redisTemplate;
-
-  /** The Redis hash operations object. */
-//  private HashOperations<String, String, Long> operations;
 
   /** The Redis operations object. */
   private ValueOperations<String, Long> operations;
@@ -59,9 +41,7 @@ public class RedisReplayCheckerStorageContainer implements ReplayCheckerStorageC
   public RedisReplayCheckerStorageContainer(final String name, final RedisTemplate<String, Long> redisTemplate) {
     this.name = Objects.requireNonNull(name, "name must not be null");
     this.redisTemplate = Objects.requireNonNull(redisTemplate, "redisTemplate must not be null");
-    //this.operations = this.redisTemplate.opsForHash();    
     this.operations = this.redisTemplate.opsForValue();
-    
   }
 
   /** {@inheritDoc} */
